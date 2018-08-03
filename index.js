f2ef747bf4618c0788fc5ff067b08f34dfa4e4ef
Seb Gertz <sebgertz@gmail.com>
2018-08-03 18:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://api.github.com/'
-const repo = 'scirocco21/js-ajax-fetch-lab-v-000'
+const myRepo = 'scirocco21/js-ajax-fetch-lab-v-000'
 
 function forkRepo() {
   const learnRepo = 'learn-co-curriculum/javascript-fetch-lab'
